078158206f70f1cca191e16fcc94fb40fabd006a
Roman Slonov <destabiliseus@gmail.com>
2019-03-26 13:40:32 +0800

    Export Avatar component as VAvatar

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as VAlert } from './Alert';
+export { default as VAvatar } from './Avatar';
 export { default as VButton } from './Button';
 export { default as VTextbox } from './Textbox';
